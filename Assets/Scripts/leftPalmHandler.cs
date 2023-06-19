e36e3d0bd8f3bead263b86bc9300c971c5a87ead
unknown <scottbuaa@outlook.com>
2023-06-19 18:03:39 +0800

    aaaa

--- Assets/Scripts/leftPalmHandler.cs
+++ Assets/Scripts/leftPalmHandler.cs
@@ -4,26 +4,28 @@ using UnityEngine;
 
 public class leftPalmHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    GameObject cue;
-    GameObject leftHandIndex_a;
+
+
     private string leftHand = "Service Provider (Desktop)/GhostHands/Generic Hand_Left/baseMeshHand_Left_GRP/Elbow/Wrist/index_meta/index_a";
     private string rightHand = "Service Provider (Desktop)/GhostHands/Generic Hand_Right/baseMeshHand_Right_GRP/Elbow/Wrist/middle_meta/middle_a";
     private string rightHand_hand_thumb_middle = "Service Provider (Desktop)/GhostHands/Generic Hand_Right/baseMeshHand_Right_GRP/Elbow/Wrist/index_meta";
     private string rightHand_hand_thumb_end = "Service Provider (Desktop)/GhostHands/Generic Hand_Right/baseMeshHand_Right_GRP/Elbow/Wrist/index_meta/index_a/index_b/index_c/index_end";
+    // Start is called before the first frame update
+    GameObject cue = GameObject.Find("Obj/Pool");
+    GameObject whiteBall = GameObject.Find("Obj/Balls/Ball_00");
+    GameObject leftHandIndex_a;
+    GameObject rightHand_thumb = GameObject.Find("Service Provider (Desktop)/GhostHands/Generic Hand_Right/baseMeshHand_Right_GRP/Elbow/Wrist/middle_meta/middle_a");
+    
     float speed = 1.0f;
     private Vector3 startPosition;
     public void setPos()
     {
         if(leftHandIndex_a.transform.position.x > 3 && leftHandIndex_a.transform.position.x < 6.5 && leftHandIndex_a.transform.position.z > 3.5 && leftHandIndex_a.transform.position.z < 8.5)
         {
-            startPosition = GameObject.Find(rightHand).GetComponent<Transform>().position;
-            cue.transform.position = (GameObject.Find(leftHand).GetComponent<Transform>().position + 10 * GameObject.Find(rightHand).GetComponent<Transform>().position) / 11;
-            Vector3 targetRotation = GameObject.Find(rightHand_hand_thumb_end).GetComponent<Transform>().position
-                - GameObject.Find(rightHand_hand_thumb_middle).GetComponent<Transform>().position;
-            Quaternion rotation = Quaternion.LookRotation(targetRotation, Vector3.forward);
-            cue.transform.rotation = rotation;
-            // cue.transform.rotation = Quaternion.FromToRotation(cue.transform.up, targetPosition);
+            Vector3 alignHand = rightHand_thumb.transform.position;
+            alignHand.y = whiteBall.transform.position.y;
+            Vector3 direction = whiteBall.transform.position - alignHand;
+            cue.transform.rotation = Quaternion.LookRotation(new Vector3(0, -1, 0), direction);
         }
     }
 
@@ -32,15 +34,12 @@ public class leftPalmHandler : MonoBehaviour
     {
         if (leftHandIndex_a.transform.position.x > 3 && leftHandIndex_a.transform.position.x < 6.5 && leftHandIndex_a.transform.position.z > 3.5 && leftHandIndex_a.transform.position.z < 8.5)
         {
-            cue.transform.position = startPosition + (GameObject.Find(rightHand).GetComponent<Transform>().position - startPosition) * 3;
-            GameObject.Find("Obj/Pool").GetComponent<Rigidbody>().velocity = 5 * GameObject.Find(rightHand).GetComponent<Rigidbody>().velocity;
-            // cue.transform.position = startPosition +
-            //    new Vector3((GameObject.Find(rightHand).GetComponent<Transform>().position.x - startPosition.x) * 1.5f, (GameObject.Find(rightHand).GetComponent<Transform>().position.y - startPosition.y) * 1.5f, (GameObject.Find(rightHand).GetComponent<Transform>().position.z - startPosition.z) * 3);
-            //      Vector3 targetPosition = GameObject.Find(leftHand).GetComponent<Transform>().position - GameObject.Find(rightHand).GetComponent<Transform>().position;
-            //      cue.transform.rotation = Quaternion.FromToRotation(cue.transform.up, targetPosition);
-            cue.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezePositionZ;
+            Vector3 alignHand = rightHand_thumb.transform.position;
+            alignHand.y = whiteBall.transform.position.y;
+            Vector3 direction = whiteBall.transform.position - alignHand;
+            cue.transform.rotation = Quaternion.LookRotation(new Vector3(0, -1, 0), direction);
         }
-        }
+     }
 
     void Start()
     {
