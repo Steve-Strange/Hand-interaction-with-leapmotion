8558c95adb83f4d58e5936703548bba00cfdac8d
Sorcerer Inferior <2694630726@qq.com>
2023-06-19 18:02:03 +0800

    w1

--- Assets/Scripts/leftPalmHandler.cs
+++ Assets/Scripts/leftPalmHandler.cs
@@ -7,16 +7,22 @@ public class leftPalmHandler : MonoBehaviour
     // Start is called before the first frame update
     GameObject cue;
     GameObject leftHandIndex_a;
+    GameObject PlayerCamera;
     private string leftHand = "Service Provider (Desktop)/GhostHands/Generic Hand_Left/baseMeshHand_Left_GRP/Elbow/Wrist/index_meta/index_a";
     private string rightHand = "Service Provider (Desktop)/GhostHands/Generic Hand_Right/baseMeshHand_Right_GRP/Elbow/Wrist/middle_meta/middle_a";
     private string rightHand_hand_thumb_middle = "Service Provider (Desktop)/GhostHands/Generic Hand_Right/baseMeshHand_Right_GRP/Elbow/Wrist/index_meta";
     private string rightHand_hand_thumb_end = "Service Provider (Desktop)/GhostHands/Generic Hand_Right/baseMeshHand_Right_GRP/Elbow/Wrist/index_meta/index_a/index_b/index_c/index_end";
     float speed = 1.0f;
     private Vector3 startPosition;
+    Vector3 tempCameraPosition;
+    bool flag = false;
     public void setPos()
     {
         if(leftHandIndex_a.transform.position.x > 3 && leftHandIndex_a.transform.position.x < 6.5 && leftHandIndex_a.transform.position.z > 3.5 && leftHandIndex_a.transform.position.z < 8.5)
         {
+            flag = true;
+            tempCameraPosition = PlayerCamera.transform.position;
+            PlayerCamera.transform.position = new Vector3(1, 1, 1);
             startPosition = GameObject.Find(rightHand).GetComponent<Transform>().position;
             cue.transform.position = (GameObject.Find(leftHand).GetComponent<Transform>().position + 10 * GameObject.Find(rightHand).GetComponent<Transform>().position) / 11;
             Vector3 targetRotation = GameObject.Find(rightHand_hand_thumb_end).GetComponent<Transform>().position
@@ -39,8 +45,15 @@ public class leftPalmHandler : MonoBehaviour
             //      Vector3 targetPosition = GameObject.Find(leftHand).GetComponent<Transform>().position - GameObject.Find(rightHand).GetComponent<Transform>().position;
             //      cue.transform.rotation = Quaternion.FromToRotation(cue.transform.up, targetPosition);
             cue.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezePositionZ;
+            print(tempCameraPosition);
         }
-        }
+    }
+
+    public void OffPos()
+    {
+        if(flag) PlayerCamera.transform.position = tempCameraPosition;
+        flag = false;
+    }
 
     void Start()
     {
@@ -51,5 +64,6 @@ public class leftPalmHandler : MonoBehaviour
     void Update()
     {
         leftHandIndex_a = GameObject.Find(leftHand);
+        PlayerCamera = GameObject.Find("FirstPersonController/Joint/PlayerCamera");
     }
 }
